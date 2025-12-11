c0419a4746faa16f6aa8aaaaa88b1ba3e1fe9dfb
StevanusAndika <stevanus0602@gmail.com>
2025-12-11 14:28:44 +0700

    ubah registration  detail

--- resources/views/dashboard/admin/registrations/registration-detail.blade.php
+++ resources/views/dashboard/admin/registrations/registration-detail.blade.php
@@ -705,7 +705,7 @@
                         </div>
                     @endif
 
-                    <a href="{{ route('admin.transactions.show') }}"
+                    <a href="{{ route('admin.transactions.index') }}"
                         class="inline-flex items-center justify-center w-full px-4 py-2.5 bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium rounded-lg transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                         <i class="fas fa-credit-card mr-2"></i>
                         Lihat Transaksi
